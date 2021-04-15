9f96da821e2784f058da163bb269b4929d9a9a7d
Enrico Battistella <enrico.battistella@digitalrockers.it>
2021-04-15 11:33:19 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const morgan = require('morgan');
 const app = express();
 const routes = require('./api/router');
@@ -9,8 +8,7 @@ const cors = require('cors');
 app.use(cors());
 // body parser
 
-app.set('view engine', 'ejs');
-app.use('/api', morgan('tiny'));
+app.use(morgan('tiny'));
 app.use('/api', routes);
 
 // error handling
